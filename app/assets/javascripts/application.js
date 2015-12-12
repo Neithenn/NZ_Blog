1e1a85d93af99ad37313425f96671079b4bedede
Neithenn <ezequiel.r26@gmail.com>
2015-12-11 23:11:19 -0300

    js remove

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,33 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-
-$(document).ready(function(){
-
-
-alert("holahola");
-
-	var showChar = 100;
-	var ellipsestext = "...";
-	var moretext = "more";
-	var lesstext = "less";
-	$('.content').each(function() {
-		var content = $(this).html();
-
-		if(content.length > showChar) {
-
-			var c = content.substr(0, showChar);
-			var h = content.substr(showChar-1, content.length - showChar);
-
-			var html = c + '<span class="moreelipses">'+ellipsestext+'</span>&nbsp;<span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">'+moretext+'</a></span>';
-
-			$(this).html(html);
-		}
-
-	
-
-
-
-
-});
