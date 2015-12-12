2661dd8d1333b1382bb8ef341de845fa250ef806
Neithenn <ezequiel.r26@gmail.com>
2015-12-12 15:48:15 -0300

    pruebas

--- db/migrate/20150528110329_create_comments.rb
+++ db/migrate/20150528110329_create_comments.rb
@@ -1,5 +1,17 @@
-class CreateComments < ActiveRecord::Migration
+
+class CreatePosts < ActiveRecord::Migration
   def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :subtitle
+      t.text :content
+      t.string :img
+      t.string :category
+
+      t.timestamps null: false
+    end
+  end
+    def change
     create_table :comments do |t|
       t.string :name
       t.string :body
@@ -9,3 +21,4 @@ class CreateComments < ActiveRecord::Migration
     end
   end
 end
+
