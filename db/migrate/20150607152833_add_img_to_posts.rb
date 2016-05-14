ad3a8757846c7cc0f80faf8b5c37ba86baa189a8
Neithenn <ezequiel.r26@gmail.com>
2016-05-14 19:00:31 -0300

    agregar todoen add img

--- db/migrate/20150607152833_add_img_to_posts.rb
+++ db/migrate/20150607152833_add_img_to_posts.rb
@@ -1,5 +1,24 @@
 class AddImgToPosts < ActiveRecord::Migration
   def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :subtitle
+      t.text :content
+      t.string :img
+      t.string :category
+      t.timestamps null: false
+
+        end
+
+   create_table :comments do |t|
+      t.string :name
+      t.string :body
+      t.references :post, index: true, foreign_key: true
+
+      t.timestamps null: false
+    
+    end
+  end
   #  add_column :posts, :img, :string
   end
-end
+
